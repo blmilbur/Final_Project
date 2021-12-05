5b995f88964cb4b21e9cc9147ff0385d58dd69e3
blmilbur <84473453+blmilbur@users.noreply.github.com>
2021-12-05 17:04:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,114 @@ document.body.querySelector(".MenuIcon").addEventListener("click", function() {
     }
 
 })
+
+document.body.querySelector(".InfoContainer1").addEventListener("click", function() {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        document.body.querySelector(".StandAloneContainer1").style.visibility = "hidden";
+        document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+
+
+    } else {
+        document.body.querySelector(".InfoWrapper").style.visibility = "hidden";
+        document.body.querySelector(".StandAloneContainer1").style.visibility = "visible";
+        window.scrollTo(1, 1);
+        document.body.querySelector(".StandAloneContainer1").addEventListener("click", function() {
+            if(!toggle) {
+                document.body.querySelector(".StandAloneContainer1").style.visibility = "hidden";
+                document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+            }
+        })
+    }
+
+})
+document.body.querySelector(".InfoContainer2").addEventListener("click", function() {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        document.body.querySelector(".StandAloneContainer2").style.visibility = "hidden";
+        document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+
+
+    } else {
+        document.body.querySelector(".InfoWrapper").style.visibility = "hidden";
+        document.body.querySelector(".StandAloneContainer2").style.visibility = "visible";
+        window.scrollTo(1, 1);
+        document.body.querySelector(".StandAloneContainer2").addEventListener("click", function() {
+            if(!toggle) {
+                document.body.querySelector(".StandAloneContainer2").style.visibility = "hidden";
+                document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+            }
+        })
+    }
+
+})
+document.body.querySelector(".InfoContainer3").addEventListener("click", function() {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        document.body.querySelector(".StandAloneContainer3").style.visibility = "hidden";
+        document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+
+
+    } else {
+        document.body.querySelector(".InfoWrapper").style.visibility = "hidden";
+        document.body.querySelector(".StandAloneContainer3").style.visibility = "visible";
+        window.scrollTo(1, 1);
+        document.body.querySelector(".StandAloneContainer3").addEventListener("click", function() {
+            if(!toggle) {
+                document.body.querySelector(".StandAloneContainer3").style.visibility = "hidden";
+                document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+            }
+        })
+    }
+
+})
+document.body.querySelector(".InfoContainer4").addEventListener("click", function() {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        document.body.querySelector(".StandAloneContainer4").style.visibility = "hidden";
+        document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+
+
+    } else {
+        document.body.querySelector(".InfoWrapper").style.visibility = "hidden";
+        document.body.querySelector(".StandAloneContainer4").style.visibility = "visible";
+        window.scrollTo(1, 1);
+        document.body.querySelector(".StandAloneContainer4").addEventListener("click", function() {
+            if(!toggle) {
+                document.body.querySelector(".StandAloneContainer4").style.visibility = "hidden";
+                document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+            }
+        })
+    }
+
+})
+document.body.querySelector(".InfoContainer5").addEventListener("click", function() {
+
+    toggle = !toggle;
+
+    if (toggle) {
+        document.body.querySelector(".StandAloneContainer5").style.visibility = "hidden";
+        document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+
+
+    } else {
+        document.body.querySelector(".InfoWrapper").style.visibility = "hidden";
+        document.body.querySelector(".StandAloneContainer5").style.visibility = "visible";
+        window.scrollTo(1, 1);
+        document.body.querySelector(".StandAloneContainer5").addEventListener("click", function() {
+            if(!toggle) {
+                document.body.querySelector(".StandAloneContainer5").style.visibility = "hidden";
+                document.body.querySelector(".InfoWrapper").style.visibility = "visible";
+            }
+        })
+    }
+
+})
